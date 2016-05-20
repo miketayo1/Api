d3b61229e528e81281d12fff94bb459a2fbb611e
miketayo <miketayo@yahoo.com>
2016-05-20 18:43:06 +0100

    api.html

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
+ 
 $(document).ready(function() {
+	var name = $('username').val();
+	$('#submit').on('click',function(){
     $.ajax({
-        url:  "https://api.github.com/users/a"
+        url:  "https://api.github.com/users/"+name
     }).then(function(data) {
        console.log(data);
-       $('.user id').append(data.id)
-       $('.avatar_url.').append(data.avatar_url);
+       $('.user_id').append(data.id)
+       $('.avatar_url').append(data.avatar_url);
     });
+    return false
+});
 });
